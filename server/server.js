9bc6e5b52f678e4a077283969e99f5d6a6439e84
MR-Titus02 <titusroxsan@gmail.com>
2025-06-13 15:53:13 +0530

    added helmet configuration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 import authRoutes from './routes/auth.routes.js';
 import mysql from 'mysql2/promise';
 import userRoutes from './routes/user.routes.js';
@@ -20,8 +21,24 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }));
 app.use(express.json());
+app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", 'https://apis.google.com'], // allow external if needed
+          styleSrc: ["'self'", 'https:', "'unsafe-inline'"],
+          imgSrc: ["'self'", 'data:', 'https:'],
+        },
+      },
+      crossOriginEmbedderPolicy: false, // if using third-party embeds like iframes
+    })
+  );
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
